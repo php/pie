3323693a182ad460d9a028755b38837a2ee52b6a
James Titcumb <james@asgrim.com>
2024-03-08 21:19:55 +0000

    Rewire some Composer dependencies and console IO

--- src/Container.php
+++ src/Container.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace Php\Pie;
 
-use Composer\Factory;
+use Composer\Composer;
+use Composer\Factory as ComposerFactory;
+use Composer\IO\ConsoleIO;
+use Composer\IO\IOInterface;
 use Composer\IO\NullIO;
 use Composer\Repository\CompositeRepository;
 use Composer\Repository\PlatformRepository;
@@ -13,6 +16,7 @@ use Composer\Repository\RepositorySet;
 use Composer\Util\AuthHelper;
 use Composer\Util\Platform;
 use GuzzleHttp\Client;
+use Illuminate\Container\Container as IlluminateContainer;
 use Php\Pie\Command\DownloadCommand;
 use Php\Pie\DependencyResolver\DependencyResolver;
 use Php\Pie\DependencyResolver\ResolveDependencyWithComposer;
@@ -22,13 +26,37 @@ use Php\Pie\Downloading\ExtractZip;
 use Php\Pie\Downloading\UnixDownloadAndExtract;
 use Psr\Container\ContainerInterface;
 use RuntimeException;
+use Symfony\Component\Console\Helper\HelperSet;
+use Symfony\Component\Console\Input\ArgvInput;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\ConsoleOutput;
+use Symfony\Component\Console\Output\OutputInterface;
 
 final class Container
 {
     public static function factory(): ContainerInterface
     {
-        $container = new \Illuminate\Container\Container();
+        $container = new IlluminateContainer();
+        $container->instance(InputInterface::class, new ArgvInput());
+        $container->instance(OutputInterface::class, new ConsoleOutput());
+
         $container->singleton(DownloadCommand::class);
+
+        $container->singleton(IOInterface::class, static function (ContainerInterface $container): IOInterface {
+            return new ConsoleIO(
+                $container->get(InputInterface::class),
+                $container->get(OutputInterface::class),
+                new HelperSet([]),
+            );
+        });
+        $container->singleton(Composer::class, static function (ContainerInterface $container): Composer {
+            $io       = $container->get(IOInterface::class);
+            $composer = ComposerFactory::create($io);
+            $io->loadConfiguration($composer->getConfig());
+
+            return $composer;
+        });
+
         $container->singleton(
             DependencyResolver::class,
             static function (): DependencyResolver {
@@ -43,15 +71,14 @@ final class Container
         );
         $container->singleton(
             UnixDownloadAndExtract::class,
-            static function (): UnixDownloadAndExtract {
-                $config = Factory::createConfig();
-                $io     = new NullIO();
-                $io->loadConfiguration($config);
-
+            static function (ContainerInterface $container): UnixDownloadAndExtract {
                 return new UnixDownloadAndExtract(
                     new DownloadZip(
                         new Client(),
-                        new AuthHelper($io, $config),
+                        new AuthHelper(
+                            $container->get(IOInterface::class),
+                            $container->get(Composer::class)->getConfig(),
+                        ),
                     ),
                     new ExtractZip(),
                 );
