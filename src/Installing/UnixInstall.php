f3de70af3c73303a74c716893ab24011bcd0ea25
James Titcumb <james@asgrim.com>
2024-07-11 16:21:57 +0100

    Write about php.ini after installing

--- src/Installing/UnixInstall.php
+++ src/Installing/UnixInstall.php
@@ -5,9 +5,12 @@ declare(strict_types=1);
 namespace Php\Pie\Installing;
 
 use Php\Pie\Downloading\DownloadedPackage;
+use Php\Pie\ExtensionType;
 use Php\Pie\Platform\TargetPlatform;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Process\Process;
+
+use function sprintf;
 
 /** @internal This is not public API for PIE, so should not be depended upon unless you accept the risk of BC breaks */
 final class UnixInstall implements Install
@@ -20,6 +23,15 @@ final class UnixInstall implements Install
 
         $output->writeln('<info>Install complete.</info>');
 
-        // @todo write info on php.ini change to make
+        /**
+         * @link https://github.com/php/pie/issues/20
+         *
+         * @todo this should be improved in future to try to automatically set up the ext
+         */
+        $output->writeln(sprintf(
+            '<comment>You must now add "%s=%s.so" to your php.ini</comment>',
+            $downloadedPackage->package->extensionType === ExtensionType::PhpModule ? 'extension' : 'zend_extension',
+            $downloadedPackage->package->extensionName->name(),
+        ));
     }
 }
