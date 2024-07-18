90384c7efb7418fc5d233f49be64c2cfb161ce50
James Titcumb <james@asgrim.com>
2024-07-18 22:59:08 +0100

    Added Windows installer integration test

--- test/integration/Installing/WindowsInstallTest.php
+++ test/integration/Installing/WindowsInstallTest.php
@@ -4,17 +4,83 @@ declare(strict_types=1);
 
 namespace Php\PieIntegrationTest\Installing;
 
+use Composer\Util\Platform;
+use Php\Pie\DependencyResolver\Package;
+use Php\Pie\Downloading\DownloadedPackage;
+use Php\Pie\ExtensionName;
+use Php\Pie\ExtensionType;
 use Php\Pie\Installing\WindowsInstall;
+use Php\Pie\Platform\Architecture;
+use Php\Pie\Platform\OperatingSystem;
+use Php\Pie\Platform\TargetPhp\PhpBinaryPath;
+use Php\Pie\Platform\TargetPlatform;
+use Php\Pie\Platform\ThreadSafetyMode;
+use Php\Pie\Platform\WindowsCompiler;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\Console\Output\BufferedOutput;
+use Symfony\Component\Process\Process;
+
+use function dirname;
+use function str_replace;
+
+use const DIRECTORY_SEPARATOR;
 
 #[CoversClass(WindowsInstall::class)]
 final class WindowsInstallTest extends TestCase
 {
-    private const TEST_EXTENSION_PATH = __DIR__ . '/../../assets/pie_test_ext';
+    private const TEST_EXTENSION_PATH = __DIR__ . '/../../assets/pie_test_ext_win';
 
     public function testWindowsInstallCanInstallExtension(): void
     {
-        self::markTestIncomplete(__METHOD__ . ' - to be implemented');
+        if (! Platform::isWindows()) {
+            self::markTestSkipped('Test can only be run on Windows');
+        }
+
+        $downloadedPackage = DownloadedPackage::fromPackageAndExtractedPath(
+            new Package(
+                ExtensionType::PhpModule,
+                ExtensionName::normaliseFromString('pie_test_ext'),
+                'php/pie-test-ext',
+                '1.2.3',
+                null,
+                [],
+            ),
+            self::TEST_EXTENSION_PATH,
+        );
+        $output            = new BufferedOutput();
+        $targetPlatform    = new TargetPlatform(
+            OperatingSystem::Windows,
+            PhpBinaryPath::fromCurrentProcess(),
+            Architecture::x86_64,
+            ThreadSafetyMode::ThreadSafe,
+            WindowsCompiler::VS16,
+        );
+        $phpPath           = dirname($targetPlatform->phpBinaryPath->phpBinaryPath);
+        $extensionPath     = $targetPlatform->phpBinaryPath->extensionPath();
+
+        $installer = new WindowsInstall();
+
+        $installedDll = $installer->__invoke($downloadedPackage, $targetPlatform, $output);
+        self::assertSame($extensionPath . '\php_pie_test_ext.dll', $installedDll);
+
+        $outputString = $output->fetch();
+
+        self::assertStringContainsString('Copied DLL to: ' . $extensionPath . '\php_pie_test_ext.dll', $outputString);
+        self::assertStringContainsString('You must now add "extension=pie_test_ext" to your php.ini', $outputString);
+
+        $expectedPdb                 = str_replace('.dll', '.pdb', $installedDll);
+        $expectedSupportingDll       = $phpPath . DIRECTORY_SEPARATOR . 'supporting-library.dll';
+        $expectedSupportingOtherFile = $phpPath . DIRECTORY_SEPARATOR . 'extras' . DIRECTORY_SEPARATOR . 'pie_test_ext' . DIRECTORY_SEPARATOR . 'README.md';
+
+        self::assertFileExists($installedDll);
+        self::assertFileExists($expectedPdb);
+        self::assertFileExists($expectedSupportingDll);
+        self::assertFileExists($expectedSupportingOtherFile);
+
+        (new Process(['del', $installedDll]))->mustRun();
+        (new Process(['del', $expectedPdb]))->mustRun();
+        (new Process(['del', $expectedSupportingDll]))->mustRun();
+        (new Process(['del', $expectedSupportingOtherFile]))->mustRun();
     }
 }
