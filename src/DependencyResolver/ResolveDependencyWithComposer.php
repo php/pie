c88aed6c5ae4fe1ab4961aa5b5dd8005401f4d09
James Titcumb <james@asgrim.com>
2024-08-01 15:37:27 +0100

    Improve version resolution to use stabilities properly in ResolveDependencyWithComposer

--- src/DependencyResolver/ResolveDependencyWithComposer.php
+++ src/DependencyResolver/ResolveDependencyWithComposer.php
@@ -4,40 +4,56 @@ declare(strict_types=1);
 
 namespace Php\Pie\DependencyResolver;
 
+use Composer\Composer;
 use Composer\Package\CompletePackageInterface;
 use Composer\Package\Version\VersionSelector;
+use Composer\Repository\CompositeRepository;
 use Composer\Repository\RepositorySet;
 use Php\Pie\ExtensionType;
 use Php\Pie\Platform\TargetPhp\ResolveTargetPhpToPlatformRepository;
 use Php\Pie\Platform\TargetPlatform;
 
 use function preg_match;
+use function str_starts_with;
 
 /** @internal This is not public API for PIE, so should not be depended upon unless you accept the risk of BC breaks */
 final class ResolveDependencyWithComposer implements DependencyResolver
 {
     public function __construct(
-        private readonly RepositorySet $repositorySet,
+        private readonly Composer $composer,
         private readonly ResolveTargetPhpToPlatformRepository $resolveTargetPhpToPlatformRepository,
     ) {
     }
 
-    public function __invoke(TargetPlatform $targetPlatform, string $packageName, string|null $requestedVersion): Package
+    private function factoryRepositorySet(string|null $requestedVersion): RepositorySet
     {
-        $preferredStability = 'stable';
-        $repoSetFlags       = 0;
+        $minimumStability = 'stable';
 
         /** Stability options from {@see https://getcomposer.org/doc/04-schema.md#minimum-stability} */
-        if ($requestedVersion !== null && preg_match('#@(dev|alpha|beta|RC|stable)$#', $requestedVersion, $matches)) {
-            $preferredStability = $matches[1];
-            $repoSetFlags      |= RepositorySet::ALLOW_UNACCEPTABLE_STABILITIES;
+        if ($requestedVersion !== null) {
+            if (preg_match('#@(dev|alpha|beta|RC|stable)$#', $requestedVersion, $matches)) {
+                $minimumStability = $matches[1];
+            }
+
+            // If a specific stability was not requested, but the version requested was `dev-` something, change to dev min stability
+            if (! $matches && str_starts_with($requestedVersion, 'dev-')) {
+                $minimumStability = 'dev';
+            }
         }
 
+        $repositorySet = new RepositorySet($minimumStability);
+        $repositorySet->addRepository(new CompositeRepository($this->composer->getRepositoryManager()->getRepositories()));
+
+        return $repositorySet;
+    }
+
+    public function __invoke(TargetPlatform $targetPlatform, string $packageName, string|null $requestedVersion): Package
+    {
         $package = (new VersionSelector(
-            $this->repositorySet,
+            $this->factoryRepositorySet($requestedVersion),
             ($this->resolveTargetPhpToPlatformRepository)($targetPlatform->phpBinaryPath),
         ))
-            ->findBestCandidate($packageName, $requestedVersion, $preferredStability, null, $repoSetFlags);
+            ->findBestCandidate($packageName, $requestedVersion);
 
         if (! $package instanceof CompletePackageInterface) {
             throw UnableToResolveRequirement::fromRequirement($packageName, $requestedVersion);
--- test/integration/DependencyResolver/ResolveDependencyWithComposerTest.php
+++ test/integration/DependencyResolver/ResolveDependencyWithComposerTest.php
@@ -13,11 +13,17 @@ use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 
+use function array_combine;
+use function array_map;
+
+use const PHP_VERSION;
+use const PHP_VERSION_ID;
+
 #[CoversClass(ResolveDependencyWithComposer::class)]
 final class ResolveDependencyWithComposerTest extends TestCase
 {
     /**
-     * @return array<non-empty-string, array{0: non-empty-string|null, 1: non-empty-string}>
+     * @return array<non-empty-string|'null', array{0: non-empty-string|null, 1: non-empty-string}>
      *
      * @psalm-suppress PossiblyUnusedMethod https://github.com/psalm/psalm-plugin-phpunit/issues/131
      */
@@ -29,32 +35,32 @@ final class ResolveDependencyWithComposerTest extends TestCase
             ['1.0.1-alpha.3@alpha', '1.0.1-alpha.3'],
             ['^1.0', '1.0.1'],
             ['^1.1.0@alpha', '1.1.0-beta.1'],
-            ['^1.0@beta', '1.1.0-beta.1'],
+            ['^1.0@beta', '1.0.1'],
             ['^1.1@beta', '1.1.0-beta.1'],
             ['~1.0.0', '1.0.1'],
             ['~1.0.0@alpha', '1.0.1'],
             ['~1.0.0@beta', '1.0.1'],
-            ['~1.0@beta', '1.1.0-beta.1'],
-            // @todo https://github.com/php/pie/issues/13 - in theory, these could work, on NonWindows at least
-//            ['dev-main@dev', 'dev-main'],
-//            ['dev-main#769f906413d6d1e12152f6d34134cbcd347ca253@dev', 'dev-main'],
+            ['~1.0@beta', '1.0.1'],
+            ['dev-main', 'dev-main'],
+            // @todo this is resolving simply to `dev-main`, but we should be able to download a specific commit
+//            ['dev-769f906413d6d1e12152f6d34134cbcd347ca253#769f906413d6d1e12152f6d34134cbcd347ca253@dev', ''],
         ];
 
         return array_combine(
-            array_map(static fn ($item) => $item[0], $versionsAndExpected),
+            array_map(static fn ($item) => $item[0] ?? 'null', $versionsAndExpected),
             $versionsAndExpected,
         );
     }
 
     #[DataProvider('validVersionsList')]
-    public function testDependenciesAreResolvedToExpectedVersions(string|null $requestedVersion, string $expectedVersion)
+    public function testDependenciesAreResolvedToExpectedVersions(string|null $requestedVersion, string $expectedVersion): void
     {
         if (PHP_VERSION_ID < 80300 || PHP_VERSION_ID >= 80400) {
             self::markTestSkipped('This test can only run on PHP 8.3 - you are running ' . PHP_VERSION);
         }
 
         $container = Container::factory();
-        $resolve = $container->get(DependencyResolver::class);
+        $resolve   = $container->get(DependencyResolver::class);
 
         $package = $resolve->__invoke(
             TargetPlatform::fromPhpBinaryPath(PhpBinaryPath::fromCurrentProcess()),
