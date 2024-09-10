91590f506191b5bb22a085e687b39a83640da879
James Titcumb <james@asgrim.com>
2024-09-10 20:25:18 +0100

    Use separate test for PIE install integration test on Windows vs non-Windows

--- test/integration/Command/InstallCommandTest.php
+++ test/integration/Command/InstallCommandTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Php\PieIntegrationTest\Command;
 
+use Composer\Util\Platform;
 use Php\Pie\Command\InstallCommand;
 use Php\Pie\Container;
 use PHPUnit\Framework\Attributes\CoversClass;
@@ -35,10 +36,14 @@ class InstallCommandTest extends TestCase
         $this->commandTester = new CommandTester(Container::factory()->get(InstallCommand::class));
     }
 
-    public function testInstallCommandWillInstallCompatibleExtension(): void
+    public function testInstallCommandWillInstallCompatibleExtensionNonWindows(): void
     {
         if (PHP_VERSION_ID < 80300 || PHP_VERSION_ID >= 80400) {
             self::markTestSkipped('This test can only run on PHP 8.3 - you are running ' . PHP_VERSION);
+        }
+
+        if (Platform::isWindows()) {
+            self::markTestSkipped('This test can only run on non-Windows systems');
         }
 
         $this->commandTester->execute(['requested-package-and-version' => self::TEST_PACKAGE]);
@@ -67,5 +72,36 @@ class InstallCommandTest extends TestCase
         }
 
         (new Process($rmCommand))->mustRun();
+    }
+
+    public function testInstallCommandWillInstallCompatibleExtensionWindows(): void
+    {
+        if (PHP_VERSION_ID < 80300 || PHP_VERSION_ID >= 80400) {
+            self::markTestSkipped('This test can only run on PHP 8.3 - you are running ' . PHP_VERSION);
+        }
+
+        if (! Platform::isWindows()) {
+            self::markTestSkipped('This test can only run on Windows systems');
+        }
+
+        $this->commandTester->execute(['requested-package-and-version' => self::TEST_PACKAGE]);
+
+        $this->commandTester->assertCommandIsSuccessful();
+
+        $outputString = $this->commandTester->getDisplay();
+        self::assertStringContainsString('Copied DLL to: ', $outputString);
+        self::assertStringContainsString('You must now add "extension=example_pie_extension" to your php.ini', $outputString);
+
+        if (
+            ! preg_match('#^Copied DLL to: (.*)$#m', $outputString, $matches)
+            || ! array_key_exists(1, $matches)
+            || $matches[1] === ''
+            || ! file_exists($matches[1])
+            || ! is_file($matches[1])
+        ) {
+            return;
+        }
+
+        (new Process(['rm', $matches[1]]))->mustRun();
     }
 }
