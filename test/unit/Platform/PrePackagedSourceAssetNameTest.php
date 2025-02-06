70639a71a47df22fbfa238d8addad80c296b51d9
James Titcumb <james@asgrim.com>
2025-02-06 09:18:53 +0000

    Add remaining unit tests for download URL override

--- test/unit/Platform/PrePackagedSourceAssetNameTest.php
+++ test/unit/Platform/PrePackagedSourceAssetNameTest.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 namespace Php\PieUnitTest\Platform;
 
+use Composer\Package\CompletePackageInterface;
+use Php\Pie\DependencyResolver\Package;
+use Php\Pie\ExtensionName;
+use Php\Pie\ExtensionType;
 use Php\Pie\Platform\PrePackagedSourceAssetName;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\TestCase;
@@ -13,6 +17,21 @@ final class PrePackagedSourceAssetNameTest extends TestCase
 {
     public function testPackageNames(): void
     {
-        self::markTestIncomplete('todo'); // @todo
+        self::assertSame(
+            [
+                'php_foobar-1.2.3-src.tgz',
+                'php_foobar-1.2.3-src.zip',
+            ],
+            PrePackagedSourceAssetName::packageNames(
+                new Package(
+                    $this->createMock(CompletePackageInterface::class),
+                    ExtensionType::PhpModule,
+                    ExtensionName::normaliseFromString('foobar'),
+                    'foo/bar',
+                    '1.2.3',
+                    null,
+                ),
+            ),
+        );
     }
 }
