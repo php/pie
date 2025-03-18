0e97b99057b317ef3f0edc99b865f653e4bc3e8e
James Titcumb <james@asgrim.com>
2025-03-18 21:56:30 +0000

    Added fallback approach for verifying downloaded PIE phar provenance
    Co-authored-by: Tim Düsterhus <tim@tideways-gmbh.com>

--- src/SelfManage/Verify/FailedToVerifyRelease.php
+++ src/SelfManage/Verify/FailedToVerifyRelease.php
@@ -12,6 +12,11 @@ use function sprintf;
 
 class FailedToVerifyRelease extends RuntimeException
 {
+    public static function fromInvalidSubjectDefinition(): self
+    {
+        return new self('Invalid subject definition in attestation payload');
+    }
+
     public static function fromMissingAttestation(ReleaseMetadata $releaseMetadata, BinaryFile $file): self
     {
         return new self(sprintf(
@@ -19,5 +24,19 @@ class FailedToVerifyRelease extends RuntimeException
             $releaseMetadata->tag,
             $file->checksum,
         ));
+    }
+
+    public static function fromSignatureVerificationFailed(int $attestationIndex, ReleaseMetadata $releaseMetadata): self
+    {
+        return new self(sprintf(
+            'Failed to verify DSSE Envelope payload signature for attestation %d for %s',
+            $attestationIndex,
+            $releaseMetadata->tag,
+        ));
+    }
+
+    public static function fromNoOpenssl(): self
+    {
+        return new self('Unable to verify without `gh` CLI tool, or openssl extension.');
     }
 }
--- src/SelfManage/Verify/VerifyPieReleaseUsingAttestation.php
+++ src/SelfManage/Verify/VerifyPieReleaseUsingAttestation.php
@@ -7,8 +7,25 @@ namespace Php\Pie\SelfManage\Verify;
 use Composer\Downloader\TransportException;
 use Composer\Util\AuthHelper;
 use Composer\Util\HttpDownloader;
+use OpenSSLAsymmetricKey;
 use Php\Pie\File\BinaryFile;
 use Php\Pie\SelfManage\Update\ReleaseMetadata;
+use Symfony\Component\Console\Output\OutputInterface;
+use Webmozart\Assert\Assert;
+
+use function array_key_exists;
+use function base64_decode;
+use function count;
+use function extension_loaded;
+use function is_array;
+use function json_decode;
+use function openssl_pkey_get_public;
+use function openssl_verify;
+use function sprintf;
+use function strlen;
+use function wordwrap;
+
+use const OPENSSL_ALGO_SHA256;
 
 /** @internal This is not public API for PIE, so should not be depended upon unless you accept the risk of BC breaks */
 final class VerifyPieReleaseUsingAttestation implements VerifyPiePhar
@@ -20,12 +37,11 @@ final class VerifyPieReleaseUsingAttestation implements VerifyPiePhar
     ) {
     }
 
-    public function verify(ReleaseMetadata $releaseMetadata, BinaryFile $pharFilename): void
+    public function verify(ReleaseMetadata $releaseMetadata, BinaryFile $pharFilename, OutputInterface $output): void
     {
-        throw new FailedToVerifyRelease('todo');
-
         // @todo prefer this $this->verifyUsingGhCli();
-        $this->verifyUsingOpenSSL($releaseMetadata, $pharFilename);
+
+        $this->verifyUsingOpenSSL($releaseMetadata, $pharFilename, $output);
     }
 
     private function verifyUsingGhCli(): void
@@ -33,19 +49,78 @@ final class VerifyPieReleaseUsingAttestation implements VerifyPiePhar
         // @todo verify using `gh attestation verify` etc
     }
 
-    private function verifyUsingOpenSSL(ReleaseMetadata $releaseMetadata, BinaryFile $pharFilename): void
+    private function verifyUsingOpenSSL(ReleaseMetadata $releaseMetadata, BinaryFile $pharFilename, OutputInterface $output): void
     {
-        // @todo require openssl
+        if (! extension_loaded('openssl')) {
+            throw FailedToVerifyRelease::fromNoOpenssl();
+        }
+
+        $output->writeln(
+            'Falling back to basic verification. To use full verification, install the `gh` CLI tool.',
+            OutputInterface::VERBOSITY_VERBOSE,
+        );
 
         $attestations = $this->downloadAttestations($releaseMetadata, $pharFilename);
 
-        /**
-         * @link https://github.com/cli/cli/blob/234d2effd545fb9d72ea77aa648caa499aecaa6e/pkg/cmd/attestation/verify/verify.go#L225-L256
-         *
-         * @todo verify the signature against the certificate
-         */
+        foreach ($attestations['attestations'] as $attestationIndex => $attestation) {
+            /**
+             * Useful references. Whilst we don't do the full verification that
+             * `gh attestation verify` would (since we don't want to re-invent
+             * the wheel), we can do some basic check of the DSSE Envelope.
+             * We'll check the payload digest matches our expectation, and
+             * verify the signature with the certificate.
+             *
+             *  - https://github.com/cli/cli/blob/234d2effd545fb9d72ea77aa648caa499aecaa6e/pkg/cmd/attestation/verify/verify.go#L225-L256
+             *  - https://docs.sigstore.dev/logging/verify-release/
+             *  - https://github.com/secure-systems-lab/dsse/blob/master/protocol.md#protocol
+             */
+            $payload        = base64_decode($attestation['bundle']['dsseEnvelope']['payload']);
+            $signature      = base64_decode($attestation['bundle']['dsseEnvelope']['signatures'][0]['sig']);
+            $decodedPayload = json_decode($payload, true);
+
+            if (
+                ! array_key_exists('subject', $decodedPayload)
+                || ! is_array($decodedPayload['subject'])
+                || count($decodedPayload['subject']) !== 1
+                || ! array_key_exists(0, $decodedPayload['subject'])
+                || ! array_key_exists('name', $decodedPayload['subject'][0])
+                || $decodedPayload['subject'][0]['name'] !== 'pie.phar'
+                || ! array_key_exists('digest', $decodedPayload['subject'][0])
+                || ! is_array($decodedPayload['subject'][0]['digest'])
+                || ! array_key_exists('sha256', $decodedPayload['subject'][0]['digest'])
+            ) {
+                throw FailedToVerifyRelease::fromInvalidSubjectDefinition();
+            }
+
+            $pharFilename->verifyAgainstOther(new BinaryFile(
+                $pharFilename->filePath,
+                $decodedPayload['subject'][0]['digest']['sha256'],
+            ));
+
+            $publicKey = openssl_pkey_get_public(
+                "-----BEGIN CERTIFICATE-----\n"
+                . wordwrap($attestation['bundle']['verificationMaterial']['certificate']['rawBytes'], 67, "\n", true) . "\n"
+                . "-----END CERTIFICATE-----\n",
+            );
+            Assert::isInstanceOf($publicKey, OpenSSLAsymmetricKey::class);
+
+            $preAuthenticationEncoding = sprintf(
+                'DSSEv1 %d %s %d %s',
+                strlen($attestation['bundle']['dsseEnvelope']['payloadType']),
+                $attestation['bundle']['dsseEnvelope']['payloadType'],
+                strlen($payload),
+                $payload,
+            );
+
+            if (openssl_verify($preAuthenticationEncoding, $signature, $publicKey, OPENSSL_ALGO_SHA256) !== 1) {
+                throw FailedToVerifyRelease::fromSignatureVerificationFailed($attestationIndex, $releaseMetadata);
+            }
+        }
+
+        $output->writeln('<info>✅ Verified the new PIE (using fallback verification)</info>');
     }
 
+    /** @return array<array-key, mixed> */
     private function downloadAttestations(ReleaseMetadata $releaseMetadata, BinaryFile $pharFilename): array
     {
         $attestationUrl = $this->githubApiBaseUrl . '/orgs/php/attestations/sha256:' . $pharFilename->checksum;
