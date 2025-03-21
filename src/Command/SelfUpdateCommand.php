f23156408c9dba257fa826995a154e024818ec34
James Titcumb <james@asgrim.com>
2025-03-21 08:48:55 +0000

    Handle logic for nightly releases if requested

--- src/Command/SelfUpdateCommand.php
+++ src/Command/SelfUpdateCommand.php
@@ -12,6 +12,7 @@ use Php\Pie\ComposerIntegration\PieComposerRequest;
 use Php\Pie\ComposerIntegration\QuieterConsoleIO;
 use Php\Pie\File\SudoFilePut;
 use Php\Pie\SelfManage\Update\FetchPieReleaseFromGitHub;
+use Php\Pie\SelfManage\Update\ReleaseMetadata;
 use Php\Pie\SelfManage\Verify\FailedToVerifyRelease;
 use Php\Pie\SelfManage\Verify\VerifyPieReleaseUsingAttestation;
 use Php\Pie\Util\PieVersion;
@@ -19,6 +20,7 @@ use Psr\Container\ContainerInterface;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 use function file_get_contents;
@@ -36,6 +38,8 @@ use const DIRECTORY_SEPARATOR;
 )]
 final class SelfUpdateCommand extends Command
 {
+    private const OPTION_NIGHTLY_UPDATE = 'nightly';
+
     /** @param non-empty-string $githubApiBaseUrl */
     public function __construct(
         private readonly string $githubApiBaseUrl,
@@ -50,6 +54,12 @@ final class SelfUpdateCommand extends Command
         parent::configure();
 
         CommandHelper::configurePhpConfigOptions($this);
+        $this->addOption(
+            self::OPTION_NIGHTLY_UPDATE,
+            null,
+            InputOption::VALUE_NONE,
+            'Update to the latest nightly version.',
+        );
     }
 
     public function execute(InputInterface $input, OutputInterface $output): int
@@ -75,21 +85,39 @@ final class SelfUpdateCommand extends Command
         $fetchLatestPieRelease = new FetchPieReleaseFromGitHub($this->githubApiBaseUrl, $httpDownloader, $authHelper);
         $verifyPiePhar         = VerifyPieReleaseUsingAttestation::factory($this->githubApiBaseUrl, $httpDownloader, $authHelper);
 
-        $latestRelease = $fetchLatestPieRelease->latestReleaseMetadata();
-        $pieVersion    = PieVersion::get();
+        if ($input->hasOption(self::OPTION_NIGHTLY_UPDATE) && $input->getOption(self::OPTION_NIGHTLY_UPDATE)) {
+            $latestRelease = new ReleaseMetadata(
+                'nightly',
+                'https://php.github.io/pie/pie-nightly.phar',
+            );
 
-        $output->writeln(sprintf('You are currently running PIE version %s', $pieVersion));
+            $output->writeln('Downloading the latest nightly release.');
+        } else {
+            $latestRelease = $fetchLatestPieRelease->latestReleaseMetadata();
+            $pieVersion    = PieVersion::get();
 
-        if (! Semver::satisfies($latestRelease->tag, '> ' . $pieVersion)) {
-            $output->writeln('<info>You already have the latest version 😍</info>');
+            if (preg_match('/^(?<tag>.+)@(?<hash>[a-f0-9]{7})$/', $pieVersion, $matches)) {
+                // Have to change the version to something the Semver library understands
+                $pieVersion = sprintf('dev-main#%s', $matches['hash']);
+                $output->writeln(sprintf(
+                    'It looks like you are running a nightly build; if you want to get the newest nightly, specify the --%s flag.',
+                    self::OPTION_NIGHTLY_UPDATE,
+                ));
+            }
 
-            return Command::SUCCESS;
+            $output->writeln(sprintf('You are currently running PIE version %s', $pieVersion));
+
+            if (! Semver::satisfies($latestRelease->tag, '> ' . $pieVersion)) {
+                $output->writeln('<info>You already have the latest version 😍</info>');
+
+                return Command::SUCCESS;
+            }
+
+            $output->writeln(
+                sprintf('Newer version %s found, going to update you... ⏳', $latestRelease->tag),
+                OutputInterface::VERBOSITY_VERBOSE,
+            );
         }
-
-        $output->writeln(
-            sprintf('Newer version %s found, going to update you... ⏳', $latestRelease->tag),
-            OutputInterface::VERBOSITY_VERBOSE,
-        );
 
         $pharFilename = $fetchLatestPieRelease->downloadContent($latestRelease);
 
