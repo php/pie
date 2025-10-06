60f3a0154995cece3c7f788f1a1ce3923fd4c6f9
James Titcumb <james@asgrim.com>
2025-10-06 20:59:33 +0100

    Added testing for libraries present on test harness

--- src/ComposerIntegration/PhpBinaryPathBasedPlatformRepository.php
+++ src/ComposerIntegration/PhpBinaryPathBasedPlatformRepository.php
@@ -124,7 +124,7 @@ class PhpBinaryPathBasedPlatformRepository extends PlatformRepository
     private function detectLibraryWithPkgConfig(string $alias, string $library): void
     {
         try {
-            $pkgConfigResult = Process::run(['pkg-config', '--print-provides', '--print-errors', $library]);
+            $pkgConfigResult = Process::run(['pkg-config', '--print-provides', '--print-errors', $library], timeout: 10);
         } catch (ProcessFailedException) {
             return;
         }
