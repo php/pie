ded16e607c31cd8ff930c19652d374ae4810d939
James Titcumb <james@asgrim.com>
2025-10-11 11:44:00 +0100

    Validate schema for PIE settings file

--- src/Settings.php
+++ src/Settings.php
@@ -4,15 +4,17 @@ declare(strict_types=1);
 
 namespace Php\Pie;
 
+use Composer\Json\JsonFile;
 use Php\Pie\SelfManage\Update\Channel;
 
 use function array_key_exists;
 use function file_exists;
 use function file_get_contents;
 use function file_put_contents;
-use function is_array;
 use function json_decode;
 use function json_encode;
+use function mkdir;
+use function rtrim;
 
 use const DIRECTORY_SEPARATOR;
 use const JSON_PRETTY_PRINT;
@@ -27,16 +29,39 @@ use const JSON_THROW_ON_ERROR;
  */
 class Settings
 {
-    private const PIE_SETTINGS_FILE_NAME = 'pie-settings.json';
+    private const PIE_SETTINGS_SCHEMA_FILE_NAME = __DIR__ . '/../resources/pie-settings-schema.json';
+    private const PIE_SETTINGS_FILE_NAME        = 'pie-settings.json';
 
     public function __construct(private readonly string $pieWorkingDirectory)
     {
     }
 
+    private function pieSettingsFullPath(): string
+    {
+        $workDir = rtrim($this->pieWorkingDirectory, DIRECTORY_SEPARATOR);
+
+        if (! file_exists($workDir)) {
+            mkdir($workDir, recursive: true);
+        }
+
+        return $workDir . DIRECTORY_SEPARATOR . self::PIE_SETTINGS_FILE_NAME;
+    }
+
+    /** @phpstan-assert PieSettings $settingsBlob */
+    private function validateSchema(mixed $settingsBlob): void
+    {
+        JsonFile::validateJsonSchema(
+            self::PIE_SETTINGS_FILE_NAME,
+            $settingsBlob,
+            JsonFile::STRICT_SCHEMA,
+            self::PIE_SETTINGS_SCHEMA_FILE_NAME,
+        );
+    }
+
     /** @phpstan-return PieSettings */
     private function read(): array
     {
-        $pieSettingsFileName = $this->pieWorkingDirectory . DIRECTORY_SEPARATOR . self::PIE_SETTINGS_FILE_NAME;
+        $pieSettingsFileName = $this->pieSettingsFullPath();
         if (! file_exists($pieSettingsFileName)) {
             return [];
         }
@@ -48,18 +73,17 @@ class Settings
 
         $config = json_decode($content, true, flags: JSON_THROW_ON_ERROR);
 
-        // @todo schema validation
+        $this->validateSchema($config);
 
-        return is_array($config) ? $config : [];
+        return $config;
     }
 
-    /** @param PieSettings $config */
+    /** @param array<array-key, mixed> $config */
     private function write(array $config): void
     {
-        // @todo schema validation
+        $this->validateSchema($config);
 
-        $pieSettingsFileName = $this->pieWorkingDirectory . DIRECTORY_SEPARATOR . self::PIE_SETTINGS_FILE_NAME;
-        file_put_contents($pieSettingsFileName, json_encode($config, flags: JSON_PRETTY_PRINT | JSON_THROW_ON_ERROR));
+        file_put_contents($this->pieSettingsFullPath(), json_encode($config, flags: JSON_PRETTY_PRINT | JSON_THROW_ON_ERROR));
     }
 
     public function updateChannel(): Channel
